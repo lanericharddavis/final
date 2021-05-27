db4dff279f684fcfc8e862c94a68d52e800392ea
Lane <lanericharddavis@gmail.com>
2021-05-27 10:57:15 -0600

    setup complete

---
+++ final.server/wwwroot/js/chunk-2d0c19e3.19de84cc.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c19e3"],{"478b":function(e,n,t){"use strict";t.r(n);var a=t("7a23");const c={class:"about"},o=Object(a["f"])("h1",null,"This is the about page",-1);function u(e,n,t,u,s,b){return Object(a["o"])(),Object(a["d"])("div",c,[o])}var s={name:"AboutPage"};s.render=u;n["default"]=s}}]);
