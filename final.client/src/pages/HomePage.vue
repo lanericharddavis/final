d9258018ba0a6b5e3aac282703e32ba2aa87f7d6
Lane <lanericharddavis@gmail.com>
2021-05-30 16:22:44 -0600

    masonry issues

--- final.client/src/pages/HomePage.vue
+++ final.client/src/pages/HomePage.vue
@@ -1,11 +1,50 @@
 <template>
   <div class="container-fluid home">
-    <div class="back">
-      <img src="../assets/img/RevillaLooks.png" class=" masonry-with-flex shadow" alt="filler img">
-      <div class="keep-title text-light text-right">
-        <p>
-          keep title
-        </p>
+    <div class="col-md-4 card-column masonry">
+      <div class="card item shadow rounded-corners my-3">
+        <img src="../assets/img/boat.png" class="rounded-corners" alt="">
+        <div class="card-img-overlay d-flex align-items-end justify-content-between">
+          <h4 class="card-title text-light">
+            Keep Title
+          </h4>
+          <img src="//placehold.it/50x50" class="circle-pic" alt="keeps owner profile picture">
+        </div>
+      </div>
+      <div class="card item shadow rounded-corners my-3">
+        <img src="../assets/img/RevillaLooks.png" class="rounded-corners" alt="">
+        <div class="card-img-overlay d-flex align-items-end justify-content-between">
+          <h4 class="card-title text-light">
+            Keep Title
+          </h4>
+          <img src="//placehold.it/50x50" class="circle-pic" alt="keeps owner profile picture">
+        </div>
+      </div>
+      <div class="card item shadow rounded-corners my-3">
+        <img src="../assets/img/fall-mtn.png" class="rounded-corners" alt="">
+        <div class="card-img-overlay d-flex align-items-end justify-content-between">
+          <h4 class="card-title text-light">
+            Keep Title
+          </h4>
+          <img src="//placehold.it/50x50" class="circle-pic" alt="keeps owner profile picture">
+        </div>
+      </div>
+      <div class="card item shadow rounded-corners my-3">
+        <img src="../assets/img/fishingpole.png" class="rounded-corners" alt="">
+        <div class="card-img-overlay d-flex align-items-end justify-content-between">
+          <h4 class="card-title text-light">
+            Keep Title
+          </h4>
+          <img src="//placehold.it/50x50" class="circle-pic" alt="keeps owner profile picture">
+        </div>
+      </div>
+      <div class="card item shadow rounded-corners my-3">
+        <img src="../assets/img/fleet.png" class="rounded-corners" alt="">
+        <div class="card-img-overlay d-flex align-items-end justify-content-between">
+          <h4 class="card-title text-light">
+            Keep Title
+          </h4>
+          <img src="//placehold.it/50x50" class="circle-pic" alt="keeps owner profile picture">
+        </div>
       </div>
     </div>
   </div>
@@ -18,15 +57,84 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home{
-  text-align: center;
-  user-select: none;
-  > img{
-    height: 200px;
-    width: 200px;
-  }
+
+.masonry { /* Masonry container */
+    -webkit-column-count: 4;
+  -moz-column-count:4;
+  column-count: 4;
+  -webkit-column-gap: 1em;
+  -moz-column-gap: 1em;
+  column-gap: 1em;
+   margin: 1.5em;
+    padding: 0;
+    -moz-column-gap: 1.5em;
+    -webkit-column-gap: 1.5em;
+    column-gap: 1.5em;
+    font-size: .85em;
+}
+.item {
+    display: inline-block;
+    background: #fff;
+    margin: 0 0 1.5em;
+    width: 100%;
+  -webkit-transition:1s ease all;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-shadow: 2px 2px 4px 0 #ccc;
+}
+.item img{max-width:100%;}
+
+@media only screen and (max-width: 320px) {
+    .masonry {
+        -moz-column-count: 1;
+        -webkit-column-count: 1;
+        column-count: 1;
+    }
 }
 
+@media only screen and (min-width: 321px) and (max-width: 768px){
+    .masonry {
+        -moz-column-count: 2;
+        -webkit-column-count: 2;
+        column-count: 2;
+    }
+}
+@media only screen and (min-width: 769px) and (max-width: 1200px){
+    .masonry {
+        -moz-column-count: 3;
+        -webkit-column-count: 3;
+        column-count: 3;
+    }
+}
+@media only screen and (min-width: 1201px) {
+    .masonry {
+        -moz-column-count: 4;
+        -webkit-column-count: 4;
+        column-count: 4;
+    }
+}
+
+// .card-columns {
+//   @include media-breakpoint-only(lg) {
+//     column-count: 4;
+//   }
+//   @include media-breakpoint-only(xl) {
+//     column-count: 5;
+//   }
+// }
+
+// .home{
+//   text-align: center;
+//   user-select: none;
+//   > img{
+//     height: 200px;
+//     width: 200px;
+//   }
+// }
+.rounded-corners {
+  border-radius: 10px;
+}
 .gradient {
   background-image: linear-gradient(to top, rgba(0, 0, 0, 0.281) , transparent);
   background-size: cover;
@@ -35,7 +143,20 @@ export default {
 
 .keep-title {
   position: absolute;
-  bottom: 50px;
+  bottom: 1px;
+  padding-left: 10px;
+}
+
+.raise-row {
+  margin-bottom: 100px;
+}
+
+.keep-img {
+  width: 300px;
+}
+
+.circle-pic{
+  border-radius: 50%;
 }
 
 </style>
